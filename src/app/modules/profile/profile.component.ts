41f8ef7d8568027fbb978c521b77c59ebb55cec7
Maria Alexandra Yukyi Chang Fonseca <59944424+alexandrayuyi@users.noreply.github.com>
2024-09-12 14:14:41 -0400

    [FEAT] map and location searcher and save button styles

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
+  myMap: any;
+  markers: any[] = [];
+  query: string = '';
+  dataDirection: any[] = [];
+  displayDataDirection: any[] = [];
 
+  ngOnInit() {
+    this.initializeMap();
+  }
+
+  initializeMap() {
+    this.myMap = L.map(this.mapContainer.nativeElement).setView([10.2363953, -67.9649982], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      subdomains: ['a', 'b', 'c'],
+      detectRetina: true
+    }).addTo(this.myMap);
+
+    this.myMap.on('click', (e: any) => {
+      this.removeExistingMarkers();
+      this.addNewMarker(e.latlng);
+      this.reverseSearch(e.latlng.lat, e.latlng.lng);
+    });
+  }
+
+  search() {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&q=${this.query}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.dataDirection = data.map((item: any) => {
+          return { lat: item.lat, lon: item.lon };
+        });
+        this.displayDataDirection = data.map((item: any) => {
+          const address = item.address;
+          const locationComponents = ['city', 'state', 'county', 'province'];
+          const location = locationComponents.find(component => address[component] !== undefined);
+          return location ? `${address[location]}, ${address['country']}` : `${address['country']}`;
+        });
+      });
+  }
+
+  moveMap(event: any) {
+    const latlng = { lat: this.dataDirection[0].lat, lon: this.dataDirection[0].lon };
+    this.myMap.setView([latlng.lat, latlng.lon], 13);
+    this.removeExistingMarkers();
+    this.addNewMarker(latlng);
+  }
+
+  reverseSearch(lat: number, lon: number) {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.address);
+      });
+  }
+
+  removeExistingMarkers() {
+    this.markers.forEach(marker => this.myMap.removeLayer(marker));
+    this.markers = [];
+  }
+
+  addNewMarker(latlng: { lat: number, lon: number }) {
+    const marker = L.marker([latlng.lat, latlng.lon]);
+    marker.addTo(this.myMap);
+    this.markers.push(marker);
+  }
 }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -5,6 +5,10 @@ import { ProfileComponent } from './profile.component';
 import { LayoutModule } from '../layout/layout.module';
 import { FooterComponent } from "../layout/components/footer/footer.component";
 import { CountrySelectComponent } from './components/country-select/country-select.component';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms';
+import { BottomNavbarComponent } from "../layout/components/bottom-navbar/bottom-navbar.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
 
 
 @NgModule({
@@ -17,6 +21,10 @@ import { CountrySelectComponent } from './components/country-select/country-sele
     ProfileRoutingModule,
     LayoutModule,
     FooterComponent,
+    AutoCompleteModule,
+    FormsModule,
+    BottomNavbarComponent,
+    ButtonComponent
 ]
 })
 export class ProfileModule { }
