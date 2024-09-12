4a2065a651b1a2de63e27d31a9811496510654d1
Maria Alexandra Yukyi Chang Fonseca <59944424+alexandrayuyi@users.noreply.github.com>
2024-09-12 10:51:49 -0400

    [FEAT] profile info form with the dashboard layout

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,7 +1,14 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { LayoutRoutingModule } from './layout-routing.module';
-@NgModule({ imports: [LayoutRoutingModule, AngularSvgIconModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class LayoutModule {}
+
+@NgModule({
+    imports: [LayoutRoutingModule, AngularSvgIconModule.forRoot(),NavbarComponent,SidebarComponent,FooterComponent],
+    exports: [NavbarComponent, SidebarComponent], // Export components
+    providers: [provideHttpClient(withInterceptorsFromDi())]
+  })
+  export class LayoutModule {}
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { LayoutModule } from '../layout/layout.module';
+import { FooterComponent } from "../layout/components/footer/footer.component";
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { ProfileComponent } from './profile.component';
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule
-  ]
+    ProfileRoutingModule,
+    LayoutModule,
+    FooterComponent
+]
 })
 export class ProfileModule { }
