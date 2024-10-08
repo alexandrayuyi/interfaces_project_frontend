117367db985107cff717ae20ee93e8d1efe1d2e3
xJDvmPWSH <juandiegovaldesm03@gmail.com>
2024-10-08 00:32:57 -0400

    Update font size input ID in config component

--- src/app/modules/config/config.component.ts
+++ src/app/modules/config/config.component.ts
@@ -13,64 +13,43 @@ export class ConfigComponent implements OnInit {
   constructor(private fb: FormBuilder, private themeService:
     ThemeService) {
     this.colorForm = this.fb.group({
-      // Default color
-      color2: ['#DEFCEA'],
-      color3: ['#000200'],
+      color1: [localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')!).primary : ['#87C09D']],
+      color2: [localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')!).secondary : ['#DEFCEA']],
+      color3: [localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')!).muted : ['#000200']],
       titleFont: [null],
       paragraphFont: [null],
-      paragraphSize: [16],
       h1Size: [localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')!).h1Size : 24], // Default h1 size
       h2Size: [localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')!).h2Size : 20], // Default h2 size
       pSize: [localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')!).pSize : 16],  // Default p size
     });
   }
 
+
   ngOnInit() {
     this.colorForm.get('color1')?.valueChanges.subscribe((color) => {
-      this.themeService.updateColors({
-        primary: this.colorForm.get('color1')?.value,
-        secondary: this.colorForm.get('color2')?.value,
-        muted: this.colorForm.get('color3')?.value
-      });
+      this.themeService.updateColors({ primary: color, secondary: this.colorForm.get('color2')?.value, muted: this.colorForm.get('color3')?.value });
     });
-
     this.colorForm.get('color2')?.valueChanges.subscribe((color) => {
-      this.themeService.updateColors({
-        primary: this.colorForm.get('color1')?.value,
-        secondary: this.colorForm.get('color2')?.value,
-        muted: this.colorForm.get('color3')?.value
-      });
+      this.themeService.updateColors({ primary: this.colorForm.get('color1')?.value, secondary: color, muted: this.colorForm.get('color3')?.value });
     });
 
     this.colorForm.get('color3')?.valueChanges.subscribe((color) => {
-      this.themeService.updateColors({
-        primary: this.colorForm.get('color1')?.value,
-        secondary: this.colorForm.get('color2')?.value,
-        muted: this.colorForm.get('color3')?.value
-      });
+      this.themeService.updateColors({ primary: this.colorForm.get('color1')?.value, secondary: this.colorForm.get('color2')?.value, muted: color });
     });
     // Subscribe to changes in font sizes
     this.colorForm.get('h1Size')?.valueChanges.subscribe((size) => {
-      this.themeService.updateFontSizes({
-        h1Size: this.colorForm.get('h1Size')?.value,
-        h2Size: this.colorForm.get('h2Size')?.value,
-        pSize: this.colorForm.get('pSize')?.value
-      });
+      this.themeService.updateFontSizes({ h1Size: this.colorForm.get('h1Size')?.value, h2Size: this.colorForm.get('h2Size')?.value, pSize: this.colorForm.get('pSize')?.value });
     });
 
     this.colorForm.get('h2Size')?.valueChanges.subscribe((size) => {
-      this.themeService.updateFontSizes({
-        h1Size: this.colorForm.get('h1Size')?.value,
-        h2Size: this.colorForm.get('h2Size')?.value,
-        pSize: this.colorForm.get('pSize')?.value
-      });
+      this.themeService.updateFontSizes({ h1Size: this.colorForm.get('h1Size')?.value, h2Size: this.colorForm.get('h2Size')?.value, pSize: this.colorForm.get('pSize')?.value });
     });
 
-    this.colorForm.get('paragraphSize')?.valueChanges.subscribe((pSize) => {
+    this.colorForm.get('pSize')?.valueChanges.subscribe((size) => {
       this.themeService.updateFontSizes({
         h1Size: this.colorForm.get('h1Size')?.value,
         h2Size: this.colorForm.get('h2Size')?.value,
-        pSize,
+        pSize: this.colorForm.get('pSize')?.value,
       });
     });
 
