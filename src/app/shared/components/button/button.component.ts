7d81c5a45075444721941bf9b06f14c02dae080e
Yuyi <alexandrayuyi@gmail.com>
2024-10-08 10:21:35 -0400

    button text dinamic

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -69,7 +69,7 @@ export class ButtonComponent implements OnInit {
 
   sizeClasses: Record<ButtonProps['size'], string> = {
     small: 'px-3 py-1 text-xs',
-    medium: 'px-5 py-2 text-sm',
+    medium: 'px-5 py-2 dynamic-p',
     large: 'px-7 py-2.5 text-lg',
   };
 
