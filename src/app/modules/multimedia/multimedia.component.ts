60d69a10a02e684b57af697e1391f44f06d0f843
Yuyi <alexandrayuyi@gmail.com>
2024-11-07 12:58:24 -0400

    [FEAT] set main layout and navigation components in multimedia page

--- src/app/modules/multimedia/multimedia.component.ts
+++ src/app/modules/multimedia/multimedia.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../layout/components/sidebar/sidebar.component';
+import { NavbarComponent } from '../layout/components/navbar/navbar.component';
+import { FooterComponent } from '../layout/components/footer/footer.component';
+import { LayoutModule } from '../layout/layout.module';
+import { BottomNavbarComponent } from "../layout/components/bottom-navbar/bottom-navbar.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-multimedia',
   templateUrl: './multimedia.component.html',
   styleUrls: ['./multimedia.component.scss'],
   standalone: true,
-  imports: [CommonModule] // Add any other necessary imports here
+  imports: [CommonModule,
+    LayoutModule,
+    ButtonComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent,
+    BottomNavbarComponent,
+    ReactiveFormsModule
+  ],
 })
-export class MultimediaComponent { }
+export class MultimediaComponent {
+  constructor(private router: Router) {}
+
+  navigateTo(page: string) {
+    this.router.navigate([`/multimedia/${page}`]);
+  }
+}
