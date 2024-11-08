9f82b4e33e011a020ab983884ea732330b92e412
Yuyi <alexandrayuyi@gmail.com>
2024-11-08 15:20:59 -0400

    [BREAKING CHANGE] delete all subroutes inside the multimedia module and only use one page

--- src/app/modules/multimedia/multimedia-routing.module.ts
+++ src/app/modules/multimedia/multimedia-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MultimediaComponent } from './multimedia.component';
-import { ImagesComponent } from './images/images.component';
-import { AudiosComponent } from './audios/audios.component';
-import { VideoComponent } from './video/video.component';
-import { PdfComponent } from './pdf/pdf.component';
-import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 const routes: Routes = [
   { path: '', component: MultimediaComponent },
-  { path: 'images', component: ImagesComponent },
-  { path: 'audios', component: AudiosComponent },
-  { path: 'video', component: VideoComponent },
-  { path: 'pdf', component: PdfComponent },
-  { path: 'terms-and-conditions', component: TermsAndConditionsComponent }
 ];
 
 @NgModule({
