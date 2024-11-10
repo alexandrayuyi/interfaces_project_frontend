25c35d3ff43735b9bd920e72c999fc5d8b938a40
Yuyi <alexandrayuyi@gmail.com>
2024-11-10 16:02:20 -0400

    [STYLES] landing 100% responsive and slider instead of carousel

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -1,40 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'slick-carousel';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements AfterViewInit {
   slides = [
     { imageSrc: '../../../assets/images/381-800x400.jpg', caption: 'Caption Text' },
     { imageSrc: '../../../assets/images/607-800x400.jpg', caption: 'Caption Two' },
-    { imageSrc: '../../../assets/images/926-800x400.jpg', caption: 'Caption Three' }
+    { imageSrc: '../../../assets/images/926-800x400.jpg', caption: 'Caption Three' },
+    { imageSrc: '../../../assets/images/381-800x400.jpg', caption: 'Caption Text' },
+    { imageSrc: '../../../assets/images/607-800x400.jpg', caption: 'Caption Two' },
+    { imageSrc: '../../../assets/images/381-800x400.jpg', caption: 'Caption Text' },
+    { imageSrc: '../../../assets/images/607-800x400.jpg', caption: 'Caption Two' }
   ];
 
-  activeIndex = 0;
-
-  ngOnInit() {
-    this.showSlides(this.activeIndex); // Initialize to show the first slide
-  }
-
-  // Increment/decrement the slide index by n
-  plusSlides(n: number) {
-    let newIndex = this.activeIndex + n;
-    if (newIndex >= this.slides.length) {
-      newIndex = 0; // Wrap back to first slide if at the end
-    } else if (newIndex < 0) {
-      newIndex = this.slides.length - 1; // Wrap back to last slide if before the first one
-    }
-    this.showSlides(newIndex);
-  }
-
-  // Show the selected slide
-  currentSlide(n: number) {
-    this.showSlides(n - 1); // `n` is 1-based, so subtract 1 for the correct index
-  }
-
-  showSlides(index: number) {
-    this.activeIndex = index;
+  ngAfterViewInit(): void {
+    ($('.responsive') as any).slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 2000,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
   }
 }
