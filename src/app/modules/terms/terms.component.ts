228d4facc96ea4e5840d68e7351ee126258380be
Yuyi <alexandrayuyi@gmail.com>
2024-11-19 11:11:14 -0400

    feat: function to show changes in terms

--- src/app/modules/terms/terms.component.ts
+++ src/app/modules/terms/terms.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
   imports: [],
   templateUrl: './terms.component.html',
-  styleUrl: './terms.component.scss'
+  styleUrls: ['./terms.component.scss']
 })
-export class TermsComponent {
+export class TermsComponent implements OnChanges {
+  @Input() htmlContent = '';
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['htmlContent']) {
+      this.updateContent();
+    }
+  }
+
+  updateContent() {
+    const wysiwygContent = document.getElementById('wysiwyg-content');
+      if (wysiwygContent && this.htmlContent) {
+        wysiwygContent.innerHTML = this.htmlContent;
+    }
+  }
 }
