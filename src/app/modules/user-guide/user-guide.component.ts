7c0a343059c316ddce758d22855e8cc7b278afc9
Yuyi <alexandrayuyi@gmail.com>
2024-11-19 21:54:10 -0400

    feat: display pdf from database

--- src/app/modules/user-guide/user-guide.component.ts
+++ src/app/modules/user-guide/user-guide.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilesService } from '../landing/services/files.service';
 
 @Component({
   selector: 'app-user-guide',
   templateUrl: './user-guide.component.html',
   styleUrl: './user-guide.component.scss'
 })
-export class UserGuideComponent {
+export class UserGuideComponent implements OnInit {
+  pdfSrc: string = '';
 
+  constructor(private filesService: FilesService) {}
+
+  ngOnInit(): void {
+    this.filesService.getFiles().subscribe(response => {
+      const pdfFile = response.data
+        .filter((file: any) => file.mimetype === 'application/pdf') // Filtrar solo PDFs
+        .map((file: any) => ({
+          pdfSrc: `http://localhost:5000/uploads/${file.filename}` // Ajusta esto según la estructura de tu respuesta
+        }))[0]; // Asumimos que solo hay un PDF
+
+      if (pdfFile) {
+        this.pdfSrc = pdfFile.pdfSrc;
+      }
+      console.log(this.pdfSrc);
+    });
+  }
 }
